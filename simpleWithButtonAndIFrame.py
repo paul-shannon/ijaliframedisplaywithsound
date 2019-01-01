343e6e52c889ef4a436c7e1219e51dbb8bbf1038
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-01-01 12:21:40 -0800

    audio files found in two different subdirs of PROJECTS, based on menu choice

--- simpleWithButtonAndIFrame.py
+++ simpleWithButtonAndIFrame.py
@@ -6,38 +6,55 @@ from flask import Flask
 import flask
 import webbrowser
 import os
-
+import pdb
 #------------------------------------------------------------------------------------------------------------------------
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-STATIC_PATH_fubar = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fubar')
-STATIC_PATH_fubar_audio = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fubar', 'audio')
 #------------------------------------------------------------------------------------------------------------------------
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-@app.server.route('/fubar/<resource>')
-def serve_static(resource):
-   return flask.send_from_directory(STATIC_PATH_fubar, resource)
+@app.server.route('/PROJECTS/<path:urlpath>')
+def serve_static_file(urlpath):
+   print("--- serve_static_file")
+   print("urlpath:  %s" % urlpath)
+   fullPath = os.path.join("PROJECTS", urlpath)
+   dirname = os.path.dirname(fullPath)
+   filename = os.path.basename(fullPath)
+   print("about to send %s, %s" % (dirname, filename))
+   return flask.send_from_directory(dirname, filename)
 
-@app.server.route('/fubar/audio/<resource>')
-def serve_static_audio(resource):
-   return flask.send_from_directory(STATIC_PATH_fubar_audio, resource)
 #------------------------------------------------------------------------------------------------------------------------
 app.scripts.config.serve_locally = True
 
+menu = dcc.Dropdown(id="menu",
+    options=[
+        {'label': 'fubar', 'value': 'fubar'},
+        {'label': 'fubar2', 'value': 'fubar2'}
+        ],
+    value="fubar",
+    style={"width": "100px", "margin": "5px"}
+    )
+
 app.layout = html.Div([
-   html.Button('Display IJAL Text', id='displayIJALTextButton', style={"margin": "20px"}),
+   html.Button('Display IJAL Text', id='displayIJALTextButton', style={"margin": "5px", "margin-top": "0px"}),
+   menu,
    html.Br(),
-   html.Iframe(id="storyIFrame", width=1200, height=800)
-   ])
+   html.Iframe(id="storyIFrame", width=1200, height=800)]
+   )
 
 #------------------------------------------------------------------------------------------------------------------------
 @app.callback(
     Output('storyIFrame', 'src'),
-    [Input('displayIJALTextButton', 'n_clicks')])
-def displayText(n_clicks):
+    [Input('displayIJALTextButton', 'n_clicks'),
+     Input('menu', 'value')]
+    )
+def displayText(n_clicks, storyName):
    if n_clicks is None:
       return("")
-   return('/fubar/test.html')
+   print("storyName: %s" % storyName)
+   if(storyName == "fubar"):
+      return('/PROJECTS/fubar/test.html')
+   elif(storyName=="fubar2"):
+      return('/PROJECTS/fubar2/daylight/test.html')
 
 #------------------------------------------------------------------------------------------------------------------------
 server = app.server
